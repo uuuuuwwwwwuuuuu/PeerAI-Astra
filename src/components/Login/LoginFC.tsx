29eaa40e6d3a221f295020b53d80bf241c552209
Denis <devorlish@gmail.com>
2025-10-23 18:08:32 +0300

    add loginFc.module.scss

--- src/components/Login/LoginFC.tsx
+++ src/components/Login/LoginFC.tsx
@@ -1,52 +1,57 @@
-import type { FC } from "react";
-import './LoginFC.scss'
-
+import type { FC } from 'react';
+import styles from './LoginFC.module.scss';
 
 interface Props {
-    type: 'login' | 'signup'
+    type: 'login' | 'signup';
 }
 
-const Login: FC<Props> = ({type}) => {
+const Login: FC<Props> = ({ type }) => {
     return (
-        <div className="form_container">
-            <form className="login_form">
-                <div className="inputs_wrapper">
-                    <div className="input_wrapper">
+        <div className={styles.form_container}>
+            <form className={styles.login_form}>
+                <div className={styles.inputs_wrapper}>
+                    <div className={styles.input_wrapper}>
                         <label htmlFor="email">Email</label>
                         <input type="email" id="email" placeholder="Enter your email address" />
                     </div>
-                    <div className="input_wrapper">
+                    <div className={styles.input_wrapper}>
                         <label htmlFor="password">Password</label>
                         <input type="password" id="password" placeholder="Enter your password" />
                     </div>
-                    <span>Forgot password? <a href="#">Send reset code</a></span>
-                    <button type="submit" className="wide_button" >{type === 'signup' ? 'Sign up' : 'Log in'}</button>
+                    <span>
+                        Forgot password? <a href="#">Send reset code</a>
+                    </span>
+                    <button type="submit" className={styles.wide_button}>
+                        {type === 'signup' ? 'Sign up' : 'Log in'}
+                    </button>
                 </div>
-                <div className="hr_wrapper">
+                <div className={styles.hr_wrapper}>
                     <div></div>
                     <span>or</span>
                     <div></div>
                 </div>
-                <div className="signup_social">
-                    <button className="wide_button">
-                        <img src='/icons/google.svg' alt="google" />
+                <div className={styles.signup_social}>
+                    <button className={styles.wide_button}>
+                        <img src="/icons/google.svg" alt="google" />
                         <span>Sign up with Google</span>
                     </button>
-                    <button className="wide_button">
-                        <img src='/icons/github.svg' alt="google" />
+                    <button className={styles.wide_button}>
+                        <img src="/icons/github.svg" alt="google" />
                         <span>Sign up with GitHub</span>
                     </button>
-                    <button className="wide_button">
+                    <button className={styles.wide_button}>
                         <span>Sign up with SSO for Enterprise</span>
                     </button>
                 </div>
-                <div className="toggle_div">
+                <div className={styles.toggle_div}>
                     <span>Don't have an account? </span>
-                    <a href={type === 'signup' ? '/login' : '/signup'}>{type === 'signup' ? 'Log in' : 'Sign up'}</a>
+                    <a href={type === 'signup' ? '/login' : '/signup'}>
+                        {type === 'signup' ? 'Log in' : 'Sign up'}
+                    </a>
                 </div>
             </form>
         </div>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
